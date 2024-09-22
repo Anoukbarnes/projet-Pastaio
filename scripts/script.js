800b8bd1094e25fec7b02e3632fdd551dbab08d2
Anoukbarnes <anouk721@gmail.com>
2024-09-22 16:54:07 -0400

    Ajout fonctionnalités menu hamburger

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,23 @@
 const navHamburger = document.getElementById('nav-hamburger');
 const navMobile = document.getElementById('nav-mobile');
+const navClose = document.getElementById('nav-close');
 
 navHamburger.addEventListener('click', () => {
     navMobile.classList.toggle('active');
+
+    if (navMobile.classList.contains('active')) {
+        navMobile.classList.remove('hidden');
+    } else {
+        navMobile.classList.add('hidden');
+    }
+});
+
+navClose.addEventListener('click', () => {
+    navMobile.classList.add('hidden');
+
+    if (navMobile.classList.contains('hidden')) {
+        navMobile.classList.remove('active');
+    } else {
+        navMobile.classList.add('active');
+    }
 });
